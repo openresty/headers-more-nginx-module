7d2db6fa01fb226f3f98a7a0ff49f479c19f951f
agentzh (章亦春) <agentzh@gmail.com>
2010-07-06 17:19:30 +0800

    enabled the no-pool-nginx patch in our build.sh script for nginx 0.8.41.

--- util/build.sh
+++ util/build.sh
@@ -6,8 +6,20 @@ root=`pwd`
 cd ~/work
 version=$1
 opts=$2
-lwp-mirror "http://sysoev.ru/nginx/nginx-$version.tar.gz" nginx-$version.tar.gz
-tar -xzvf nginx-$version.tar.gz
+
+if [ ! -s "nginx-$version.tar.gz" ]; then
+    wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
+    tar -xzvf nginx-$version.tar.gz || exit 1
+    if [ "$version" = "0.8.41" ]; then
+        cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./
+        patch -p0 < nginx-$version-no_pool.patch || exit 1
+    fi
+fi
+
+#tar -xzvf nginx-$version.tar.gz || exit 1
+#cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
+#patch -p0 < nginx-0.8.41-no_pool.patch
+
 cd nginx-$version/
 if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
     ./configure --prefix=/opt/nginx \
