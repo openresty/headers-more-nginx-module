78286ca0d804155230f98aaa7ed6ce5fb8ea765b
agentzh (章亦春) <agentzh@gmail.com>
2011-08-09 10:42:13 +0800

    confirmed that we work with nginx 1.0.5.

--- util/build.sh
+++ util/build.sh
@@ -3,61 +3,27 @@
 # this file is mostly meant to be used by the author himself.
 
 root=`pwd`
-#cd ~/work
 version=$1
-opts=$2
-target=$root/work/nginx
+force=$2
 
-if [ ! -d ./buildroot ]; then
-    mkdir ./buildroot || exit 1
-fi
-
-cd buildroot || exit 1
-
-if [ ! -s "nginx-$version.tar.gz" ]; then
-    if [ -f ~/work/nginx-$version.tar.gz ]; then
-        cp ~/work/nginx-$version.tar.gz ./ || exit 1
-    else
-        wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz || exit 1
-    fi
-
-    tar -xzvf nginx-$version.tar.gz || exit 1
-fi
-
-#tar -xzvf nginx-$version.tar.gz || exit 1
-#cp $root/../no-pool-nginx/nginx-$version-no_pool.patch ./ || exit 1
-#patch -p0 < nginx-$version-no_pool.patch || exit 1
-
-cd nginx-$version/ || exit 1
-
-if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
-    ./configure --prefix=$target \
-            --with-cc-opt="-O0 -fprofile-arcs -ftest-coverage" \
-            --with-ld-opt="-fprofile-arcs -ftest-coverage" \
-            --without-mail_pop3_module \
-            --without-mail_imap_module \
-            --without-mail_smtp_module \
-            --without-http_upstream_ip_hash_module \
-            --without-http_empty_gif_module \
-            --without-http_memcached_module \
-            --without-http_referer_module \
-            --without-http_autoindex_module \
-            --without-http_auth_basic_module \
-            --without-http_userid_module \
-          --add-module=$root/../eval-nginx-module \
-          --add-module=$root/../echo-nginx-module \
-          --add-module=$root $opts \
-        || exit 1
-          #--with-debug \
-          #--add-module=$root/../ndk-nginx-module \
+ngx-build $force $version \
+        --with-cc-opt="-O0 -fprofile-arcs -ftest-coverage" \
+        --with-ld-opt="-fprofile-arcs -ftest-coverage" \
+        --without-mail_pop3_module \
+        --without-mail_imap_module \
+        --without-mail_smtp_module \
+        --without-http_upstream_ip_hash_module \
+        --without-http_empty_gif_module \
+        --without-http_memcached_module \
+        --without-http_referer_module \
+        --without-http_autoindex_module \
+        --without-http_auth_basic_module \
+        --without-http_userid_module \
+      --add-module=$root/../eval-nginx-module \
+      --add-module=$root/../echo-nginx-module \
+      --add-module=$root $opts \
+    || exit 1
+      #--with-debug \
+      #--add-module=$root/../ndk-nginx-module \
   #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
-
-fi
-if [ -f $target/sbin/nginx ]; then
-    rm -f $target/sbin/nginx
-fi
-if [ -f $target/logs/nginx.pid ]; then
-    kill `cat $target/logs/nginx.pid`
-fi
-make -j3 && make install
 
